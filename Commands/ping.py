a3265064fe53c4fe391e10ae54625034b96854d8
joern19 <joern@keepout.de>
2020-04-10 21:56:04 +0200

    now able to ping any Server with the ping command

--- Commands/ping.py
+++ Commands/ping.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
-
+import os
+from pythonping import ping
 
 class Ping(commands.Cog):
 
@@ -10,8 +11,24 @@ class Ping(commands.Cog):
     # Ping command
     @commands.command()
     async def ping(self, ctx):
-        await ctx.send("Pong!")
+        if (len(ctx.message.content.split(" ")) == 1):
+            await ctx.send("Pong!") #wait until send..
+            return
+        else:
+            address = ctx.message.content.split(" ")[1]
+            await self.pingServer(ctx, address)
 
+    async def pingServer(self, ctx, address):
+        await ctx.send("Going to Ping: " + address)
+        try:
+            res = ping(address, count=1)
+            if (res.success):
+                await ctx.send("Ping Successful in: " +str(res.rtt_avg_ms)+ "ms")
+            else:
+                await ctx.send("Host was unreachable.")
+        except Exception as ex:
+            print(ex)
+            await ctx.send("Error, is the Hostname valid?")
 
 def setup(bot):
     bot.add_cog(Ping(bot))
